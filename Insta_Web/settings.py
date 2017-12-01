f059575c0a2055eb2fd084077123d126df9f4da5
nishaf <nishafnaeem3@gmail.com>
2017-12-01 23:31:06 +0500

    Uploaded.

--- Insta_Web/settings.py
+++ Insta_Web/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'InstagramDatabase',
         'USER': 'root',
-        'PASSWORD':'',
+        'PASSWORD':'braeden123!!',
         'HOST': '127.0.0.1',
     },
 
